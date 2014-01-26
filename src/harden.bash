f2913d8f653f75d9ec5c02d3594f0f76789711e5
Mattias Andrée <maandree@operamail.com>
2014-01-26 14:42:25 +0100

    further harden by hardcode the path to sudo
    Signed-off-by: Mattias Andrée <maandree@operamail.com>

--- src/harden.bash
+++ src/harden.bash
@@ -3,6 +3,6 @@
 # Prevents a program or shell function from pretending to be sudo
 function sudo
 {
-    "$(which sudo)" --prompt $'\e[01;32m[sudo] password for '"$USER"$': \033[00m' "$@"
+    /usr/bin/sudo --prompt $'\e[01;32m[sudo] password for '"$USER"$': \033[00m' "$@"
 }
 
