9368ac10bdbab2aede05cc21843cfc5a667f7150
Mattias Andrée <maandree@operamail.com>
2014-02-03 19:27:51 +0100

    add ls aliases with --group-directories-first
    Signed-off-by: Mattias Andrée <maandree@operamail.com>

--- src/ls-aliases.bash
+++ src/ls-aliases.bash
@@ -5,7 +5,7 @@ alias ll='ls -alF'
 
 
 #DESCRIPTION:  ls with hidden files
-#USAGE:        (ls | lsh)
+#USAGE:        (la | lsh)
 
 alias la='ls -A'
 alias lsh='ls -A'
@@ -26,4 +26,34 @@ alias lsc='ls --color=always'
 #DESCRIPTION:  Use colours in ls when the output is not piped
 
 alias ls='ls --color=auto'
+
+
+#DESCRIPTION:  ll with directories before files
+#USAGE:        ll
+
+alias lld='ls -alF --group-directories-first'
+
+
+#DESCRIPTION:  ls and lsh with directories before files
+#USAGE:        (ls | lsh)
+
+alias lad='ls -A --group-directories-first'
+alias lshd='ls -A --group-directories-first'
+
+
+#DESCRIPTION:  l with directories before files
+#USAGE:        l
+
+alias lcfd='ls -CF --group-directories-first'
+
+
+#DESCRIPTION:  lsc with directories before files
+#USAGE:        lsc
+
+alias lscd='ls --color=always --group-directories-first'
+
+
+#DESCRIPTION:  ls with directories before files
+
+alias lsd='ls --color=auto --group-directories-first'
 
